02028fe19e5158873c95f71efa1dfd6b39cf5444
gpaparaz <giorgia.paparazzo@gmail.com>
2023-11-03 14:56:41 +0100

    implmented exclusive filters

--- cocktail-bar/src/view/AllCocktails/AllCocktails.js
+++ cocktail-bar/src/view/AllCocktails/AllCocktails.js
@@ -1,87 +1,127 @@
 import React, { useEffect, useState } from "react";
 import { useGlobalContext } from "../../context";
-import style from './allcocktails.css';
-import clsx from 'clsx';
+import style from "./allcocktails.css";
+import clsx from "clsx";
 import Cocktails from "../../components/Cocktails/Cocktails";
 import Loading from "../../components/Loading/Loading";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
-import Form from 'react-bootstrap/Form';
+import Form from "react-bootstrap/Form";
 import useSearchAllDrinks from "../../useSearchAllDrinks";
 import useFetch from "../../useFetch";
 import useGetListOfFilters from "../../useGetListOfFilters";
 
 const AllCocktails = () => {
-    const {
-        data,
-        isLoading,
-        isError,
-        count,
-        searchCocktail
-      } = useGlobalContext();
-    
-      const [displayGrid, setDisplayGrid] = useState("true");
+  const { data, isLoading, isError, count, searchCocktail } =
+    useGlobalContext();
 
-      const { data:glass, isLoading: isLoadingGlass } = useGetListOfFilters('g=list');
-      const { data:category, isLoading: isLoadingCategory } = useGetListOfFilters('c=list');
-      
-      const [selectedGlass, setSelectedGlass] = useState('');
-      const [selectedCategory, setSelectedCategory] = useState('');
-    
-      const handleSelectedGlass = (e) => {
-        setSelectedGlass(e.target.value);
-        searchCocktail('g=' + e.target.value, true);
-      };
+  const [displayGrid, setDisplayGrid] = useState("true");
 
-      const handleSelectedCategory = (e) => {
-        setSelectedCategory(e.target.value);
-        searchCocktail('c=' + e.target.value, true);
-      };
+  const { data: glass, isLoading: isLoadingGlass } =
+    useGetListOfFilters("g=list");
+  const { data: category, isLoading: isLoadingCategory } =
+    useGetListOfFilters("c=list");
 
-      useEffect(() => {
-        if (selectedGlass) {
-          searchCocktail(`g=${selectedGlass}`, true);
-        }
-        else if(selectedCategory){
-          searchCocktail(`c=${selectedCategory}`, true)
-        } else {
-          //di default non sto cercando tramite nessn filtro
-          searchCocktail("", false);
-        }
-      }, [selectedGlass, searchCocktail]);
+  const [selectedGlass, setSelectedGlass] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
 
+  const handleSelectedGlass = (e) => {
+    const selectedGlass = e.target.value;
+    setSelectedGlass(selectedGlass);
+  
+    if (selectedCategory !== '') {
+      setSelectedCategory('');
+      searchCocktail(`g=${selectedGlass}`, true);
+    } else {
+      searchCocktail(`g=${selectedGlass}`, true);
+    }
+  };
+  
+  const handleSelectedCategory = (e) => {
+    const selectedCategory = e.target.value;
+    setSelectedCategory(selectedCategory);
+  
+    if (selectedGlass !== '') {
+      setSelectedGlass('');
+      searchCocktail(`c=${selectedCategory}`, true);
+    } else {
+      searchCocktail(`c=${selectedCategory}`, true);
+    }
+  };
 
-    
+  const resetFilters = () => {
+    setSelectedCategory("");
+    setSelectedGlass("");
+  };
+
+  useEffect(() => {
+    if (selectedGlass) {
+      searchCocktail(`g=${selectedGlass}`, true);
+    } else if (selectedCategory) {
+      searchCocktail(`c=${selectedCategory}`, true);
+    } else {
+      //di default non sto cercando tramite nessun filtro
+      searchCocktail("", false);
+    }
+  }, [selectedGlass, searchCocktail]);
+
+  const handleToggle = (selected) => {
+    if (!selected) {
+      setSelectedGlass('');
+      setSelectedCategory('');
+      searchCocktail('', false);
+    }
+  };
+
   return (
     <div className="d-flex">
       <div className="col-3 p-3 mb-2 filters">
-      {
-        (isLoadingGlass || isLoadingCategory) ? (
-          <Loading />):(
-        <>
-        <label>Tipo di bicchiere</label>
-        <Form>
-          <Form.Select aria-label="Select glass" onChange={handleSelectedGlass}>
-            <option value="">All</option>
-            {glass.map((glass, index) => (
-              <option key={index} value={glass.strGlass}>
-                {glass.strGlass}
-              </option>
-            ))}
-          </Form.Select>
-        </Form>
-        <label>Categoria</label>
-        <Form>
-          <Form.Select aria-label="Select category" onChange={handleSelectedCategory}>
-            <option value="">All</option>
-            {category.map((category, index) => (
-              <option key={index} value={category.strCategory}>
-                {category.strCategory}
-              </option>
-            ))}
-          </Form.Select>
-        </Form>
-        </>)
-      }
+        <button className="btn btn-secondary" onClick={resetFilters}>
+          Reset filtri{" "}
+        </button>
+        <br />
+        {isLoadingGlass || isLoadingCategory ? (
+          <Loading />
+        ) : (
+          <>
+            <Form className="my-3">
+            <label>Tipo di bicchiere</label>
+  <Form.Select
+    aria-label="Select glass"
+    onChange={(e) => {
+      handleToggle(e.target.value !== '');
+      handleSelectedGlass(e);
+    }}
+    value={selectedGlass}
+  >
+    <option value="">All</option>
+    {glass.map((glass, index) => (
+      <option key={index} value={glass.strGlass}>
+        {glass.strGlass}
+      </option>
+    ))}
+  </Form.Select>
+</Form>
+<Form className="my-3">
+<label>Categoria drink </label>
+  <Form.Select
+    aria-label="Select category"
+    onChange={(e) => {
+      handleToggle(e.target.value !== '');
+      handleSelectedCategory(e);
+    }}
+    value={selectedCategory}
+  >
+    <option value="">All</option>
+    {category.map((category, index) => (
+      <option key={index} value={category.strCategory}>
+        {category.strCategory}
+      </option>
+    ))}
+  </Form.Select>
+</Form>
+            
+          </>
+        )}
       </div>
 
       <div className="col-sm-9 p-3 content-products">
@@ -115,12 +155,12 @@ const AllCocktails = () => {
               {displayGrid ? (
                 // <Cocktails data={data} />
                 isLoading ? (
-                    <Loading />
-                  ) : isError ? (
-                    <ErrorMessage>Nessun Cocktail Trovato</ErrorMessage>
-                  ) : data && data && data.length > 0 ?(
-                    <Cocktails data={data} count={count} />
-                  ) : null
+                  <Loading />
+                ) : isError ? (
+                  <ErrorMessage>Nessun Cocktail Trovato</ErrorMessage>
+                ) : data && data && data.length > 0 ? (
+                  <Cocktails data={data} count={count} />
+                ) : null
               ) : (
                 <p> altra lista di cocktail in lista</p>
               )}
