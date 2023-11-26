e684250ce153b3f60d11ff8f745e977dc91436bf
gpaparaz <giorgia.paparazzo@gmail.com>
2023-11-26 23:25:22 +0100

    refactor single cocktail page

--- src/view/SingleCocktailPage/SingleCocktailPage.js
+++ src/view/SingleCocktailPage/SingleCocktailPage.js
@@ -62,26 +62,20 @@ const SingleCocktailPage = () => {
   ];
 
   const style = {
-    backgroundImage: ` linear-gradient(0deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${image})`,
+    backgroundImage: ` url(${image})`,
     backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
     height: "100vh",
   };
 
+
+
   return (
     <>
-      <div style={style}>
-        <Swiper
-          direction={"vertical"}
-          pagination={{
-            clickable: true,
-          }}
-          modules={[Pagination]}
-          className="mySwiper"
-        >
-          <SwiperSlide>
-            <div className="cocktail-hero">
+    <div className="d-flex">
+      <div className="col-6 d-flex justify-content-center">
+      <div className="cocktail-hero">
               <div className="spacer">
                 <h1 className="cocktailName mb-3">{name}</h1>
                 <div className="cocktail-type">
@@ -91,10 +85,16 @@ const SingleCocktailPage = () => {
                 </div>
               </div>
             </div>
-          </SwiperSlide>
-          <SwiperSlide>
-          <div className="cocktail-datails">
-            <h3 className="mb-3">Ingredienti:</h3>
+      </div>
+      <div className="col-6 " style={style}>
+      </div>
+    </div>
+    <div className="d-flex">
+    <div className="col-6 d-flex align-items-center justify-content-end instructionsBg">
+              <h1 className="instructions">Istruzioni</h1>
+      </div>
+      <div className="col-6 cocktailDetails p-5">
+      <h3 className="mb-3">Ingredienti:</h3>
             <ul className="instruction-list">
               {strInstructionsList.map((el, index) => {
                 if (el.istruzione) {
@@ -108,19 +108,20 @@ const SingleCocktailPage = () => {
                 }
               })}
             </ul>
-            </div>
-          </SwiperSlide>
 
-          <SwiperSlide>
-          <div className="cocktail-datails">
-            <h3 className="mb-3">Istruzioni:</h3>
+            <div class="divider div-transparent"></div>
+
+            <h3 className="mb-3 mt-5">Istruzioni:</h3>
             <h5 className="info">
               {strInstructionsIT ? strInstructionsIT : strInstructions}
             </h5>
-            </div>
-          </SwiperSlide>
-        </Swiper>
       </div>
+      
+
+    </div>
+
+
+      
       <div className="my-5 container-sm">
         <h3>Altri drink simili</h3>
         <div className="container-sm px-5">
