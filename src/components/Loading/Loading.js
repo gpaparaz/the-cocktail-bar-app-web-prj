00fc7ff689cdc61b3db58eb9cbac3cc206b0dde5
gpaparaz <giorgia.paparazzo@gmail.com>
2024-01-22 11:45:20 +0100

    removed unused dependencies, fix loading

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,17 +1,12 @@
 import React from "react";
+import loading from './../../assets/image/loading.gif'
+import './loading.css'
 
 const Loading = () => {
   return (
     <div className="loading-position">
-      <div className="lds-roller">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className="lds-roller d-flex justify-content-center align-items-center">
+        <img className="loading-img" src={loading} />
       </div>
     </div>
   );
